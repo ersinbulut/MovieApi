2a78910eeb3a58b7d16f47c8c87579ac0bb77019
BULUTS <BULUTS@BULUTS>
2025-02-20 20:54:15 +0300

    #10 Movie Api - AspNet Core 9.0 Onion Architecture - Api Controller Oluşturma

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/MovieHandlers/GetMovieByIdQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieApi.Application.Features.CQRSDesignPattern.Queries.MovieQueries;
+using MovieApi.Application.Features.CQRSDesignPattern.Results.MovieResults;
+using MovieApi.Persistence.Context;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers
+{
+    public class GetMovieByIdQueryHandler
+    {
+        private readonly MovieContext _context;
+
+        public GetMovieByIdQueryHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<GetMovieByIdQueryResults> Handle(GetMovieByIdQuery query)
+        {
+            var value =await _context.Movies.FindAsync(query.MovieID);
+            return new GetMovieByIdQueryResults
+            {
+                CoverImageUrl = value.CoverImageUrl,
+                CreatedYear = value.CreatedYear,
+                Description = value.Description,
+                Duration = value.Duration,
+                Rating = value.Rating,
+                RealeseDate = value.RealeseDate,
+                Status = value.Status,
+                Title = value.Title
+            };
+        }
+    }
+}
