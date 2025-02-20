8ff993ec6ce75cfa818c4aa81410498130f195b1
BULUTS <BULUTS@BULUTS>
2025-02-20 20:38:05 +0300

    #9 Movie Api - AspNet Core 9.0 Onion Architecture - Get Movie Command Handler Sınıfı

--- Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/CategoryHandlers/RemoveCategoryCommandHandler.cs
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/CategoryHandlers/RemoveCategoryCommandHandler.cs
@@ -19,8 +19,8 @@ namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.CategoryHandl
 
         public async void Handle(RemoveCategoryCommand command)
         {
-            var value = await _context.Movies.FindAsync(command.CategoryID);
-            _context.Movies.Remove(value);
+            var value = await _context.Categories.FindAsync(command.CategoryID);
+            _context.Categories.Remove(value);
             await _context.SaveChangesAsync();
         }
     }
---
+++ Presentation/MovieApi.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieApi.Application.Features.CQRSDesignPattern.Results.CategoryResults;
+
+namespace MovieApi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        //private readonly GetCategoryQueryResults
+
+    }
+}
