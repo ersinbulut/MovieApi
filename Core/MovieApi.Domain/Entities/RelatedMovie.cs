cfd6b9e2c65f9a221c45e9fa337c848409f6ef7d
Developer <Developer@DEV>
2025-05-12 17:37:36 +0300

    #32 Movie Api - AspNet Core 9.0 Onion Arc. - Yapay Zeka ve Veri Analizi ile Film Önerisi Hazırlığı

---
+++ Core/MovieApi.Domain/Entities/RelatedMovie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Domain.Entities
+{
+    public class RelatedMovie
+    {
+        public int RelatedMovieID { get; set; }
+        public int MovieID { get; set; }
+        public int UserID { get; set; }
+        public bool IsWatch { get; set; }
+    }
+}
