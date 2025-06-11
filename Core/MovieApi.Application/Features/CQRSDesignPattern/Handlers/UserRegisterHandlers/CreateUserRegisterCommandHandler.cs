a649fe29bc91f793378ea37e1c0d7c4b84f1f89d
Developer <Developer@DEV>
2025-06-11 08:51:43 +0300

    #35 Movie Api - AspNet Core 9.0 Onion Architecture - CQRS Üzerinde Api ile Register İşlemi

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/UserRegisterHandlers/CreateUserRegisterCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using MovieApi.Application.Features.CQRSDesignPattern.Commands.MovieCommands;
+using MovieApi.Application.Features.CQRSDesignPattern.Commands.UserRegisterCommands;
+using MovieApi.Domain.Entities;
+using MovieApi.Persistence.Context;
+using MovieApi.Persistence.Identity;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.UserRegisterHandlers
+{
+    public class CreateUserRegisterCommandHandler
+    {
+        private readonly MovieContext _context;
+        private readonly UserManager<AppUser> _userManager;
+
+        public CreateUserRegisterCommandHandler(MovieContext context, UserManager<AppUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task Handle(CreateUserRegisterCommand command)
+        {
+            var user = new AppUser()
+            {
+                Name = command.Name,
+                Surname = command.Surname,
+                Email = command.Email,
+                UserName = command.Username
+            };
+            await _userManager.CreateAsync(user,command.Password);
+          
+    
+        }
+    }
+}
---
+++ Presentation/MovieApi.WebApi/Controllers/RegistersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieApi.Application.Features.CQRSDesignPattern.Commands.UserRegisterCommands;
+using MovieApi.Application.Features.CQRSDesignPattern.Handlers.UserRegisterHandlers;
+
+namespace MovieApi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistersController : ControllerBase
+    {
+        private readonly CreateUserRegisterCommandHandler _createUserRegisterCommand;
+
+        public RegistersController(CreateUserRegisterCommandHandler createUserRegisterCommand)
+        {
+            _createUserRegisterCommand = createUserRegisterCommand;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUserRegister(CreateUserRegisterCommand command)
+        {
+            await _createUserRegisterCommand.Handle(command);
+            return Ok("Kullanıcı başarıyla eklendi");
+        }
+    }
+}
